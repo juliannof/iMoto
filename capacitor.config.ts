ebca3c1fb23faf4d583bb1885afe7e11489b3747
julianno <julianno@iMac-Pro.local>
2025-01-12 20:42:31 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iMoto25',
+  webDir: 'www'
+};
+
+export default config;
